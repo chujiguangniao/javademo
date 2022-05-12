34892e6f23bf13f3e0e2a1798395e366cb825013
yangchengxu <1305007718@qq.com>
2022-05-12 22:11:57 +0800

    addsomething

--- src/Cell.java
+++ src/Cell.java
@@ -1,14 +1,14 @@
 public class Cell {   //细胞类
-    //private int x,y;  //细胞位置
+    private int x,y;  //细胞位置
     private boolean isLive;  //细胞状态（true:存活  false:死亡）
 
     public Cell(){
-        //this.x=x;
-       // this.y=y;
+        this.x=x;
+        this.y=y;
         isLive=false;
     }
 
-   /* public int getX() {
+    public int getX() {
         return x;
     }
 
@@ -22,7 +22,7 @@ public class Cell {   //细胞类
 
     public void setY(int y) {
         this.y = y;
-    }*/
+    }
 
     public boolean getIsLive() {
         return isLive;
