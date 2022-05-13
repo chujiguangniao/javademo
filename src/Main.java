601a75241a4706494d6a661a9e858cc62fe62bd2
yangchengxu <1305007718@qq.com>
2022-05-13 23:20:44 +0800

    main函数注释

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     private static GameGUI Game;
     private static World world;
 
-    //操作界面
+    //操作界面，main函数
     public static void main(String arg[]){
         world=new World(20,30);
         Game=new GameGUI("Game of Life",world);
