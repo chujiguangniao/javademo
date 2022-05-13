cbb21c55e24f80c547f3c9e0b24884e43731bf70
yangchengxu <1305007718@qq.com>
2022-05-13 14:05:52 +0800

    修改注释

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     private static GameGUI Game;
     private static World world;
 
+    //操作界面
     public static void main(String arg[]){
         world=new World(20,30);
         Game=new GameGUI("Game of Life",world);
